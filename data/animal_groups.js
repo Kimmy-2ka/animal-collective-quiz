f1e78accc8ec234ce925034969a6ed39fff3a4ac
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2026-04-17 12:31:11 +0900

    easyレベルのみでクイズの基本動作を実装

---
+++ data/animal_groups.js
@@ -0,0 +1,14 @@
+const animalGroupsData = [
+  { animal: "lions", collectiveNoun: "pride", emoji: "🦁", level: "easy" },
+  { animal: "crows", collectiveNoun: "murder", emoji: "🐦‍⬛", level: "easy" },
+  { animal: "elephants", collectiveNoun: "parade", emoji: "🐘", level: "easy" },
+  { animal: "dolphins", collectiveNoun: "pod", emoji: "🐬", level: "easy" },
+  { animal: "fish", collectiveNoun: "school", emoji: "🐟", level: "easy" },
+  { animal: "cows", collectiveNoun: "herd", emoji: "🐄", level: "easy" },
+  { animal: "sheep", collectiveNoun: "flock", emoji: "🐑", level: "easy" },
+  { animal: "wolves", collectiveNoun: "pack", emoji: "🐺", level: "easy" },
+  { animal: "monkeys", collectiveNoun: "troop", emoji: "🐒", level: "easy" },
+  { animal: "geese", collectiveNoun: "gaggle", emoji: "🪿", level: "easy" },
+];
+
+export default animalGroupsData;
